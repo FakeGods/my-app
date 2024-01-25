7f1d04d6822cedd611c90fb649dabd263ee19462
FakeGods <w.leszkowicz99@gmail.com>
2024-01-25 15:36:36 +0100

    1231

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,18 @@
 
 function Navbar () {
     return (
-        <>
-        </>
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">Zosia?</div>
+                <div className="navbar_menu">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </div>
+            </div>
+        </div>
+        
     );
     }
 

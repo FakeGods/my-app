576e8f37f79c434946dd1863a08024530325174c
FakeGods <w.leszkowicz99@gmail.com>
2024-01-25 21:32:07 +0100

    123

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import About from "../components/About/About";
 
 
 
 function Home () {
     return (
-        <Navbar>
-        </Navbar>
+        <>
+        < Navbar/>
+        <About/>
+        </>
     )
 }
 export default Home;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,27 @@
+
+
+function About () {
+    return (
+        <>
+            <section className="about" id="about">
+                <div className="container">
+                    <div className="about-content">
+                        <div className="text-main">
+                            <h3>
+                                About me
+                            </h3>
+                            <h4>
+                                Coś o Zosi.
+                            </h4>
+                            <p>
+                                Mniejsze coś o Zosi.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default About;

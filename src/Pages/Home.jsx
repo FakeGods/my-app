a1bd54b5c7451afdaad4761c9ac81323b804046d
FakeGods <w.leszkowicz99@gmail.com>
2024-01-27 11:46:37 +0100

    980

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import About from "../components/About/About";
+import Projects from "../components/Projects/Projects";
+import Footer from "../components/Footer/Footer";
+import Contact from "../components/Contact/Contact";
 
 
 
@@ -9,6 +12,9 @@ function Home () {
         <>
         < Navbar/>
         < About/>
+        < Projects/>
+        < Contact/>
+        < Footer/>
         </>
     )
 }
